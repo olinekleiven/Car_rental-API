03fb08a2a279660effc2b25e561a919a2bb0748c
Sondre Garnæs <sondre433@gmail.com>
2024-10-03 09:13:11 +0200

    Added neo4j connection

---
+++ db_connect.py
@@ -0,0 +1,20 @@
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Neo4jConnection:
+    def __init__(self, uri, user, password):
+        uri= os.getenv("URI")
+        user= os.getenv("USERNAME")
+        password= os.getenv("PASSWORD")
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+    
+    def close(self):
+        self._driver.close()
+    
+    def query(self, query, parameters=None):
+        with self._driver.session() as session:
+            result = session.run(query, parameters)
+            return [record for record in result]
