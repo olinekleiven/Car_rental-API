2cccb99b4cf88882a5eda6ba5c745e4c66ad5a16
Sondre Garnæs <sondre433@gmail.com>
2024-10-03 08:56:10 +0200

    Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Car Rental API!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
